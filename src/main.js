07ff44df3d61bffe53ff18ca7bfae603df5c5fc7
Anna <anna.mironova89@yandex.ru>
2023-12-11 17:27:41 +0300

    2.14. Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import TripListPresenter from './presenter/trip-list-presenter.js';
+import EventsModel from './model/events-model.js';
 
 const contentContainerElement = document.querySelector('.trip-events');
 const filterContainerElement = document.querySelector('.trip-controls__filters');
 
-const tripListPresenter = new TripListPresenter(contentContainerElement, filterContainerElement);
+const eventsModel = new EventsModel();
+const tripListPresenter = new TripListPresenter(contentContainerElement, filterContainerElement, eventsModel);
 
 tripListPresenter.init();
